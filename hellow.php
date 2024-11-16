cf7e19851e3e85c143f006ddc15ea673f1904c35
Marcelo <marcelos35@gmail.com>
2024-11-16 13:10:10 -0300

    meu primeiro commit hellow

---
+++ hellow.php
@@ -0,0 +1,18 @@
+<?php
+
+$message = "<h1> Primeiro </h1> <strong> Paragrafo </strong> PHP...!";
+echo $message;
+
+$nota = 10;
+for ($i=0; $i < $nota; $i++){
+	echo $i;
+	echo"<br>";
+	
+};
+?>
+
+<!--<h1><?= $message ?></h1>-->
+
+<!--	for([starting_condition]; [continue_condition]; [ending_condition]) {
+		[code_block]
+	} -->
